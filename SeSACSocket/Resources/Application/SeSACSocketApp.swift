7ddf0a9c010db2fa9d015934b241f2981ac0549f
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-26 11:04:26 +0900

    rename: SocketView from ContentView

--- SeSACSocket/Resources/Application/SeSACSocketApp.swift
+++ SeSACSocket/Resources/Application/SeSACSocketApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SeSACSocketApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SocketView()
         }
     }
 }
--- SeSACSocket/Resources/Presentation/SocketView/SocketView.swift
+++ SeSACSocket/Resources/Presentation/SocketView/SocketView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SocketView.swift
 //  SeSACSocket
 //
 //  Created by Taekwon Lee on 12/26/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SocketView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    SocketView()
 }
