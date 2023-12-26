8883b285be4b370ee99eea125a58cdb8271efe5b
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-26 10:17:37 +0900

    Initial Commit

---
+++ SeSACSocket/SeSACSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACSocketApp.swift
+//  SeSACSocket
+//
+//  Created by Taekwon Lee on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSACSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
