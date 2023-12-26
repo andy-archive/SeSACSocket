db33fa1d4db83eaad2b29c4d1e4f627e22a87dba
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-26 10:54:57 +0900

    feat: openWebSocket

---
+++ SeSACSocket/Resources/Data/DataSources/WebSocketManager.swift
@@ -0,0 +1,52 @@
+//
+//  WebSocketManager.swift
+//  SeSACSocket
+//
+//  Created by Taekwon Lee on 12/26/23.
+//
+
+import Foundation
+
+final class WebSocketManager: NSObject {
+    
+    //MARK: - Singleton
+    static let shared = WebSocketManager()
+    
+    /* ⭐️ NSObject의 public init에 의해 override init으로 바뀜 */
+    private override init() { }
+    
+    //MARK: - Properties
+    private var webSocket: URLSessionWebSocketTask?
+    
+    //MARK: - Methods
+    func openWebSocket() {
+        let session = URLSession(
+            configuration: .default,
+            delegate: self, // URLSessionDelegate 채택해야 함
+            delegateQueue: nil
+        )
+        
+        if let url = URL(string: Constants.url.upbit.orderbook) {
+            webSocket = session.webSocketTask(
+                // dataTask가 아닌 webSocketTask
+                with: url
+            )
+            webSocket?.resume()
+        }
+    }
+    
+    func closeWebSocket() { }
+}
+
+/* 📝 Note
+ /* 응답을 받을 때 제약이 있음 -> 클로저 */
+ URLSession.shared.dataTask(with: <#T##URLRequest#>).resume()
+ 
+ /* configuration의 별도 설정이 필요함 */
+ //URLSession(configuration: .default).dataTask(with: <#T##URLRequest#>).resume()
+ */
+
+//MARK: - URLSession WebSocket Delegate
+extension WebSocketManager: URLSessionWebSocketDelegate {
+    
+}
---
+++ SeSACSocket/Resources/Utilities/Constants/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  SeSACSocket
+//
+//  Created by Taekwon Lee on 12/26/23.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum url {
+        
+        enum upbit {
+            
+            static let orderbook = "wss://api.upbit.com/websocket/v1"
+        }
+    }
+}
